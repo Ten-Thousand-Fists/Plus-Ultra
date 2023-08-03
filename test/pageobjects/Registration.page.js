11a1092cb7249ecec9f240efa7b45f90de1d2a6d
J0NUll <mcm-mac-17@MacMini17s-Mac-mini.local>
2023-08-03 15:25:38 -0700

    V1

--- test/pageobjects/Registration.page.js
+++ test/pageobjects/Registration.page.js
@@ -5,43 +5,65 @@ import Page from './page.js';
 /**
  * sub page containing specific selectors and methods for a specific page
  */
-class loginPage extends Page {
+class Registration extends Page {
     /**
      * define selectors using getter methods
      */
-    get name () {
+    get firstName() {
         return $('#firstName');
     }
     
-    get lastName () {
+    get lastName() {
         return $('#lastName');
     }
 
-    get emailAddress () {
+    get emailAddress() {
         return $('#emailAddress');
     }
 
-    get password () {
+    get password() {
         return $('#password');
     }
 
-    get confirmPassword () {
+    get confirmPassword() {
         return $('#confirmPassword');
     }
 
-    get address () {
+    get address() {
         return $('.registerAddress .rbFieldIsRequired');
     }
 
-    get aptSteUnit () {
+    get aptSteUnit() {
         return $('#registerForm .registerAddress:nth-child(6) [type]');
     }
     
-    get country () {
+    get country() {
         return $('select#country');
     }
 
+    get city() {
+        return $('#city');
+    }
 
+    get state() {
+        return $('select#state');
+    }
+
+    get Zip() {
+        return $('#zip');
+    }
+
+    get numberSection1() {
+        return $('#phoneNumber');
+    }
+
+    get numberSection2() {
+        return $('#phoneNumber2');
+    }
+
+    get numberSection3() {
+        return $('#phoneNumber3');
+    }
 
     /**
      * a method to encapsule automation code to interact with the page
@@ -56,8 +78,8 @@ class loginPage extends Page {
      * overwrite specific options to adapt it to page object
      */
     open () {
-        return super.open('login');
+        return super.open('Register');
     }
 }
 
-export default new loginPage();
+export default new RegistrationPage();
