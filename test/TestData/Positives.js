0029f6cd8eb8d84fd8c37202ec16b4bf89d446e2
Joshua Mitchell <pcmaster@Joshuas-MacBook-Pro.local>
2023-08-08 21:59:28 -0700

    08/08/2023

--- test/TestData/Positives.js
+++ test/TestData/Positives.js
@@ -3,43 +3,43 @@ export class Positive_vals {
 
     static Pos_case_EP = {
 
-        FirstName, "Loren",
-        Lastname, "Kim",
-        email_Address, "KimMitch3ll@gmail.com",
-        Password, "Pas5@#",
-        Confirm_Password, "Pas5@#",
-        Address, "4020 Peña-St",
-        Apt/Suite/Unit, "#J5904",
-        Country, "UNITED STATES",
-        City, "Piña",
-        State, "CA",
-        Zip, "92046",
-        Phone_Number, "714-966-6605",
-        Next_Button, "NEXT Button"
-        ,
+        1: "Loren",
+        Lastname: "Kim",
+        email_Address: "KimMitch3ll@gmail.com",
+        Password: "Pas5@#",
+        Confirm_Password: "Pas5@#",
+        Address: "4020 Peña-St",
+        Apt_Suite_Unit: "#J5904",
+        Country: "UNITED STATES",
+        City: "Piña",
+        State: "CA",
+        Zip: "92046",
+        Phone_Number: "714-966-6605",
+        Next_Button: "NEXT Button"
+        
     
     
     
     
     
-    }
+    } 
     
     
     static Pos_case_BVAL = {
     
-        FirstName, "LHK",
-        lastname, "LHK",
-        email_Address, "L@M.com",
-        Password, "B3nny!",
-        Confirm_Password, "B3nny!",
-        Address, "360A",
-        Apt/Suite/Unit, "76B",
-        Country, "UNITED STATES",
-        City, "Piña",
-        State, "CA",
-        Zip, "92046",
-        Phone_Number, "714-966-6605",
-        Next_Button, "NEXT Button",
+        FirstName: "LHK",
+        lastname: "LHK",
+        email_Address: "L@M.com",
+        Password: "B3nny!",
+        Confirm_Password: "B3nny!",
+        Address: "360A",
+        Apt_Suite_Unit: "76B",
+        Country: "UNITED STATES",
+        City: "Piña",
+        State: "CA",
+        Zip: "92046",
+        Phone_Number: "714-966-6605",
+        Next_Button: "NEXT Button"
     
     
     
@@ -50,19 +50,19 @@ export class Positive_vals {
     
     static Pos_case_BVAH = {
     
-        FirstName, "LorenLorenLorenLorenLorenLorenLorenLorenLorenLorenLorenLorenLore",
-        lastname, "KimKimKimKimKimKimKimKimKimKimKimKimKimKimKimKimKimKimKimKimKimK",
-        Email_Address, "MîchaelSmith@yahoo.com",
-        Password, "air;ohg@aiehwfoīáf32hioj23ir2iohrio2hdadIREWSDAFOJLJOJFWAFHDOSra",
-        Confirm_Password, "air;ohg@aiehwfoīáf32hioj23ir2iohrio2hdadIREWSDAFOJLJOJFWAFHDOSra",
-        Address, "12345 Papillon Avenue, 09876 Velvet Street, 90631 Hershey Road, 421 Love St",
-        Apt/Suite/Unit, "Apt 76B-A23",
-        Country, "Ontario",
-        City, "San Diego, Los Angeles, San Francisco, Tokyo, Montreal, Sacramento, Seoul, Osaka, NYC",
-        State, "CA",
-        Zip, "90213",
-        Phone_Number, "963-214-5780",
-        Next_Button, "NEXT Button",
+        FirstName: "LorenLorenLorenLorenLorenLorenLorenLorenLorenLorenLorenLorenLore",
+        lastname: "KimKimKimKimKimKimKimKimKimKimKimKimKimKimKimKimKimKimKimKimKimK",
+        Email_Address: "MîchaelSmith@yahoo.com",
+        Password: "air;ohg@aiehwfoīáf32hioj23ir2iohrio2hdadIREWSDAFOJLJOJFWAFHDOSra",
+        Confirm_Password: "air;ohg@aiehwfoīáf32hioj23ir2iohrio2hdadIREWSDAFOJLJOJFWAFHDOSra",
+        Address: "12345 Papillon Avenue, 09876 Velvet Street, 90631 Hershey Road, 421 Love St",
+        Apt_Suite_Unit: "Apt 76B-A23",
+        Country: "Ontario",
+        City: "San Diego, Los Angeles, San Francisco, Tokyo, Montreal, Sacramento, Seoul, Osaka, NYC",
+        State: "CA",
+        Zip: "90213",
+        Phone_Number: "963-214-5780",
+        Next_Button: "NEXT Button"
     
     
     }
@@ -71,14 +71,14 @@ export class Positive_vals {
 
     static refrencearray ={
 
-    firstName, "Loren",
-    lastName, "Kim"
+        firstName: "Loren",
+        lastName: "Kim"
 
 
     }
 
 
-    staticx_tags = {
+    static staticx_tags = {
 
         x1: "l"
         
--- test/specs/Positive Tests/PositiveBVA-H.spec.js
+++ test/specs/Positive Tests/PositiveBVA-H.spec.js
@@ -5,12 +5,16 @@ import RegistrationPage from '../../pageobjects/Registration.page.js'
 import { Positive_vals } from '../../TestData/Positives.js';
 
 describe('Positive test for Rallybound ', () => {
-    it('Account Registration', async () => {
+    xit('Account Registration', async () => {
         await RegistrationPage.open(); 
 
-        //await expect(RegistrationPage.firstName).toBeExisting(); 
-        //await (RegistrationPage.firstname).setValue(Positive_vals.Pos_case_EP.Firstname);
+        await expect(RegistrationPage.firstName).toBeExisting(); 
+        await browser.pause(1000);
+        await (RegistrationPage.firstname).setValue(Positive_vals.Pos_case_EP[1]);
+        await browser.pause(2000);
 
+
+        
 
 
 
