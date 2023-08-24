ab71d87f3a936ee1f1024b523b7ec39345c81056
J0NUll <mcm-mac-17@MacMini17s-Mac-mini.local>
2023-08-24 15:54:11 -0700

    08/24/23

--- test/TestData/Negatives.js
+++ test/TestData/Negatives.js
@@ -1,14 +1,14 @@
-export class Positive_vals {
+export class Negative_vals {
 
 
 
         static Reference_vals = {
 
-            firstName: "Loren",
-            lastName: "Kim",
-            emailAddress: "NatalieHawkins@gmail.com",
+            FirstName: "Loren",
+            lastname: "Kim",
+            email_Address: "NatalieHawkins@gmail.com",
             Password: "Thi$îsth3Way!",
-            confirmPassword: "Thi$îsth3Way!",
+            Confirm_Password: "Thi$îsth3Way!",
             Address: "54321 Carmel Valley Road",
             Apt_Suite_Unit: "A#650",
             Country: "UNITED STATES",
@@ -24,7 +24,571 @@ export class Positive_vals {
     } 
 
 
+
+    //First Name Negative Case Type's
     
+    static Testcase_31 = {
+        
+        TagX1: "l"
+
+    }
+
+    static Testcase_32 = {
+        
+        TagX2: "lorenlorenlorenlorenlorenlorenlorenlorenlorenlorenlorenlorenlorenloren"
+
+    }
+
+    static Testcase_33 = {
+        
+        TagX3: "L^rem"
+
+    }
+
+    static Testcase_34 = {
+        
+        TagX4: "L0ren"
+
+    }
+
+    static Testcase_35 = {
+        
+        TagX5: "" //This is supposed to be a Null input for this test case 
+
+    }
+
+    static Testcase_36 = {
+        
+        TagX6: "Lo ren Kim"
+
+    }
+
+
+
+
+
+    
+    //Last Name Negative Case Type
+
+    static Testcase_37 = {
+        
+        TagX7: "M"
+
+    }
+
+    static Testcase_38 = {
+        
+        TagX8: "dominguezdominguezdominguezdominguezdominguezdominguezdominguezdominguez"
+
+    }
+
+    static Testcase_39 = {
+        
+        TagX9: "K!m"
+
+    }
+
+    static Testcase_40 = {
+        
+        TagX10: "K1m"
+
+    }
+
+    static Testcase_41 = {
+        
+        TagX11: "" //This is supposed to be a Null input for this test case 
+
+
+    }
+
+    static Testcase_42 = {
+        
+        TagX12: "K  im"
+
+    }
+
+
+
+
+
+
+    //Email Negative test Cases 
+
+    static Testcase_43 = {
+        
+        TagX13: "N@.com"
+
+    }
+
+    static Testcase_44 = {
+        
+        TagX14: "NatalieHawkins@gmail.comNatalieHawkins@gmail.comNatalieHawkins@gm"
+
+    }
+
+    static Testcase_45 = {
+        
+        TagX15: "" //This is supposed to be a Null input for this test case 
+
+    }
+
+    static Testcase_46 = {
+        
+        TagX16: "@bennygmail.com"
+
+    }
+
+    static Testcase_47 = {
+        
+        TagX17: "Natalie@Hawkins@gmail.com"
+
+    }
+
+    static Testcase_48 = {
+        
+        TagX18: "NatalieHawkins.gmail.com"
+
+    }
+
+    static Testcase_49 = {
+        
+        TagX19: "NatalieHawkinsgmail.com@"
+
+    }
+
+    static Testcase_50 = {
+        
+        TagX20: "Natalie Hawkins @gmail.com"
+
+    }
+    
+
+
+
+
+
+
+    //Password Negative Test Cases
+
+    static Testcase_51 = {
+        
+        TagX21: "1DKf!"
+
+    }
+
+    static Testcase_52 = {
+        
+        TagX22: "$@#!$2131gGSCVSEFESVdadiafjodpfhaewiopfhaeiowfj;23$%@#$RQEFW)vagtu9j34got9ehjnrp"
+
+    }
+
+    static Testcase_53 = {
+        
+        TagX23: "" //This is supposed to be a Null input 
+
+    }
+
+    static Testcase_54 = {
+        
+        TagX24: "NatalieHawkins@gmail.com"
+
+    }
+
+    static Testcase_55 = {
+        
+        TagX25: "r!ff7even"
+
+    }
+
+    static Testcase_56 = {
+        
+        TagX26: "Yas!!!!!!!!!!"
+
+    }
+
+    static Testcase_57 = {
+        
+        TagX27: "IT'S ME!!!1"
+
+    }
+
+    static Testcase_58 = {
+        
+        TagX28: "Loren"
+
+    }
+
+    static Testcase_59 = {
+        
+        TagX29: "Kim"
+
+    }
+
+    static Testcase_60 = {
+        
+        TagX30: "Thisistheway"
+
+    }
+
+
+
+
+
+
+
+    // Negative Address test cases 
+    static Testcase_61 = {
+        
+        TagX31: "Ave"
+
+    }
+
+    static Testcase_62 = {
+        
+        TagX32: "54321CarmelValleyRoad54321CarmelValleyRoad54321CarmelValleyRoad54"
+
+    }
+
+    static Testcase_63 = {
+        
+        TagX33: "54321 Carme! Valley Road"
+
+    }
+
+
+
+
+
+
+
+    // Negative Apt test cases
+
+    static Testcase_64 = {
+        
+        TagX34: "A#"
+
+    }
+
+    static Testcase_65 = {
+        
+        TagX35: "A#650A#650A"
+
+    }
+
+    static Testcase_66 = {
+        
+        TagX36: "0#6A5"
+
+    }
+
+    static Testcase_67 = {
+        
+        TagX37: "A@650"
+
+    }
+
+
+
+
+
+
+
+    //Negative City test cases
+
+    static Testcase_68 = {
+        
+        TagX38: "USD"
+
+    }
+
+    static Testcase_69 = {
+        
+        TagX39: "SanDiegoSanDiegoSanDiegoSanDiegoSanDiegoSanDiegoSanDiegoSanDiegoS"
+
+    }
+
+    static Testcase_70 = {
+        
+        TagX40: "San Di3go"
+
+    }
+
+    static Testcase_71 = {
+        
+        TagX41: "S@n Diego"
+
+    }
+
+    static Testcase_72 = {
+        
+        TagX42: "" //This input is a Null input
+
+    }
+
+    static Testcase_73 = {
+        
+        TagX43: "" //This input is a Null input for the state 
+
+    }
+
+
+
+
+
+
+    //Negative test cases for US Zip 
+
+    static Testcase_74 = {
+        
+        TagX44: "92!10"
+
+    }
+
+    static Testcase_75 = {
+        
+        TagX45: "9211Q"
+
+    }
+
+    static Testcase_76 = {
+        
+        TagX46: "9210"
+
+    }
+
+    static Testcase_77 = {
+        
+        TagX47: "921100"
+
+    }
+
+    static Testcase_78 = {
+        
+        TagX48: "921 10"
+
+    }
+
+    static Testcase_79 = {
+        
+        TagX49: "" //This input is a Null input 
+
+    }
+
+
+
+
+
+
+
+    //Negative test cases for Canada Postal 
+
+    static Testcase_80 = {
+        
+        TagX50: "M5C 2^9"
+
+    }
+
+    static Testcase_81 = {
+        
+        TagX51: "M5Ç 2V9"
+
+    }
+
+    static Testcase_82 = {
+        
+        TagX52: "" //This input is a Null input
+
+    }
+
+    static Testcase_ = {
+        
+        TagX8: ""
+
+    }
+
+    static Testcase_38 = {
+        
+        TagX8: ""
+
+    }
+
+    static Testcase_38 = {
+        
+        TagX8: ""
+
+    }
+
+    static Testcase_38 = {
+        
+        TagX8: ""
+
+    }
+
+    static Testcase_38 = {
+        
+        TagX8: ""
+
+    }
+
+    static Testcase_38 = {
+        
+        TagX8: ""
+
+    }
+
+    static Testcase_38 = {
+        
+        TagX8: ""
+
+    }
+
+    static Testcase_38 = {
+        
+        TagX8: ""
+
+    }
+
+    static Testcase_38 = {
+        
+        TagX8: ""
+
+    }
+
+    static Testcase_38 = {
+        
+        TagX8: ""
+
+    }
+
+    static Testcase_38 = {
+        
+        TagX8: ""
+
+    }
+
+    static Testcase_38 = {
+        
+        TagX8: ""
+
+    }
+
+    static Testcase_38 = {
+        
+        TagX8: ""
+
+    }
+
+    static Testcase_38 = {
+        
+        TagX8: ""
+
+    }
+
+    static Testcase_38 = {
+        
+        TagX8: ""
+
+    }
+
+
+    
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- test/specs/PositiveBVA-H.spec.js
+++ test/specs/PositiveBVA-H.spec.js
@@ -3,11 +3,12 @@
  */
 import RegistrationPage from '../pageobjects/Registration.page.js'
 import { Positive_vals } from '../TestData/Positives.js';
+import { Negative_vals } from '../TestData/Negatives.js';
 
 describe('Positive test for Rallybound ', () => {
 
 
-    it('Account Registration', async () => {
+    xit('Account Registration', async () => {
         await RegistrationPage.open(); 
 
 
@@ -19,118 +20,33 @@ describe('Positive test for Rallybound ', () => {
         
     }); 
 
-
-
-
-    xit('Account Registration', async () => {
+    it ('Negative test', async () => {
         await RegistrationPage.open(); 
-
-        await browser.pause(1000);
-        await RegistrationPage.login(
-            Positive_vals.Pos_case_EP.firstName,
-            Positive_vals.Pos_case_EP.lastName,
-            Positive_vals.Pos_case_EP.emailAddress,
-            Positive_vals.Pos_case_EP.Password,
-            Positive_vals.Pos_case_EP.confirmPassword,
-            Positive_vals.Pos_case_EP.Address,
-            Positive_vals.Pos_case_EP.Apt_Suite_Unit,
-            Positive_vals.Pos_case_EP.Country,
-            Positive_vals.Pos_case_EP.City,
-            Positive_vals.Pos_case_EP.State,
-            Positive_vals.Pos_case_EP.Zip, 
-            Positive_vals.Pos_case_EP.Phone1, 
-            Positive_vals.Pos_case_EP.Phone2, 
-            Positive_vals.Pos_case_EP.Phone3,
-            );
+        await RegistrationPage.sort(Negative_vals.Reference_vals);
+        await RegistrationPage.first_Name.setValue(Negative_vals.Testcase_45.TagX15); 
         await browser.pause(2000);
-        await RegistrationPage.Next_btn.click(); 
-        await browser.pause(5000);
-
-    
-            
-    }); 
-
-    xit('Selection as Canada: Work in progress----', async () => {
-
-        //There are some values that were not working with province selector for automation such as the province AB
-        //Unsure about other values that do not work. 
-        //Also check if the rest of the test works in regards to the Phone1, Phone2, Phone3
-        //Also at home setup is not working with other browsers only with the edge browser check if that is also the case as well 
-        //at NFAR too. 
-
-
-        await RegistrationPage.open(); 
-
-        await browser.pause(1000);
-
-        await RegistrationPage.login2(
-            Positive_vals.Test1.firstName,
-            Positive_vals.Test1.lastName,
-            Positive_vals.Test1.emailAddress,
-            Positive_vals.Test1.Password,
-            Positive_vals.Test1.confirmPassword,
-            Positive_vals.Test1.Address,
-            Positive_vals.Test1.Apt_Suite_Unit,
-            Positive_vals.Test1.Country,
-            Positive_vals.Test1.City,
-            Positive_vals.Test1.Province,
-            Positive_vals.Test1.Postal, 
-            //Positive_vals.Test1.Phone1, 
-        //     // Positive_vals.Test1.Phone2, 
-        //     // Positive_vals.Test1.Phone3
-
-            );
-            
-
-            
-        
-        //await RegistrationPage.login2(Positive_vals.Test1.Country,Positive_vals.Test1.Province); 
-
-        await browser.pause(2000);
-    })
-
-
-
-    it('Account Registration', async () => {
-        await RegistrationPage.open(); 
-
-
-        await RegistrationPage.sort(Positive_vals.Pos_case_EP);
+        await RegistrationPage.Next_btn.click();  
         await browser.pause(4000);
-        
+        await expect (RegistrationPage.first_Name).toBeExisting(); 
 
-    
-            
+
+
+
+
     }); 
 
-    xit('Account Registration', async () => {
-    await RegistrationPage.open(); 
-
-        await browser.pause(1000);
-        await RegistrationPage.login(
-            Positive_vals.Pos_case_EP.firstName,
-            Positive_vals.Pos_case_EP.lastName,
-            Positive_vals.Pos_case_EP.emailAddress,
-            Positive_vals.Pos_case_EP.Password,
-            Positive_vals.Pos_case_EP.confirmPassword,
-            Positive_vals.Pos_case_EP.Address,
-            Positive_vals.Pos_case_EP.Apt_Suite_Unit,
-            Positive_vals.Pos_case_EP.Country,
-            Positive_vals.Pos_case_EP.City,
-            Positive_vals.Pos_case_EP.State,
-            Positive_vals.Pos_case_EP.Zip, 
-            Positive_vals.Pos_case_EP.Phone1, 
-            Positive_vals.Pos_case_EP.Phone2, 
-            Positive_vals.Pos_case_EP.Phone3,
-            );
+    it ('Negative test', async () => {
+        await RegistrationPage.open(); 
+        
         await browser.pause(2000);
-        await RegistrationPage.Next_btn.click(); 
-        await browser.pause(5000);
+        await RegistrationPage.Next_btn.click();  
+        await browser.pause(4000);
+        await expect (RegistrationPage.flash_alert).toBeExisting(); 
+
+    }); 
 
 
 
 
-
-})
 
 }); 
