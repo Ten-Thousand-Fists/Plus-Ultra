1f42cc480f7521366f1706ce42745922263899af
J0NUll <mcm-mac-17@MacMini17s-Mac-mini.local>
2023-09-15 16:28:04 -0700

    09/15/2023

--- test/TestData/Positives.js
+++ test/TestData/Positives.js
@@ -3,18 +3,18 @@ export class Positive_vals {
 
     static Reference_vals = {
 
-        FirstName: "Loren",
-        lastname: "Kim",
-        email_Address: "KimMitch3ll@gmail.com",
-        Password: "Pas5@#@#",
-        Confirm_Password: "Pas5@#@#",
-        Address: "4020 Peña-St",
-        Apt_Suite_Unit: "#J5904",
-        Country: "UNITED STATES",
-        City: "San Diego",
-        State: "CA",
-        Zip: "92046",
-        Phone1: "714",
+        FirstName: "Loren",  // Tag_V1 
+        lastname: "Kim",    // Tag_V6
+        email_Address: "KimMitch3ll@gmail.com",   //Tag_V11
+        Password: "Pas5@#@#",   // Tag_V16
+        Confirm_Password: "Pas5@#@#",   // Tag_V23
+        Address: "4020 Peña-St",   // Tag_V24
+        Apt_Suite_Unit: "#J5904",   // Tag_V29
+        Country: "UNITED STATES",   // Tag_V32
+        City: "San Diego",   // Tag_V37
+        State: "CA",   // Tag_V41
+        Zip: "92046",   // Tag_V43
+        Phone1: "714",   // Tag_V51
         Phone2: "966",
         Phone3: "6605"
     
@@ -24,8 +24,96 @@ export class Positive_vals {
     } 
 
 
+    static Canada_Vals = {
+
+        FirstName: "Loren",  // Tag_V1 
+        lastname: "Kim",    // Tag_V6
+        email_Address: "KimMitch3ll@gmail.com",   //Tag_V11
+        Password: "Pas5@#@#",   // Tag_V16
+        Confirm_Password: "Pas5@#@#",   // Tag_V23
+        Address: "4020 Peña-St",   // Tag_V24
+        Apt_Suite_Unit: "#J5904",   // Tag_V29
+        Country: "CANADA",   // Tag_V34-Tag_V35
+        City: "San Diego",   // Tag_V37
+        State: "CA",   // Tag_V41
+        Zip: "M5C 2V9",   // Tag_V45-V46
+        Phone1: "714",   // Tag_V51
+        Phone2: "966",
+        Phone3: "6605"
     
+        
+        
+        
+    } 
+
+    static France_Vals = {
+        
+        FirstName: "Loren",  // Tag_V1 
+        lastname: "Kim",    // Tag_V6
+        email_Address: "KimMitch3ll@gmail.com",   //Tag_V11
+        Password: "Pas5@#@#",   // Tag_V16
+        Confirm_Password: "Pas5@#@#",   // Tag_V23
+        Address: "4020 Peña-St",   // Tag_V24
+        Apt_Suite_Unit: "#J5904",   // Tag_V29
+        Country: "France",   // Tag_V36
+        City: "San Diego",   // Tag_V37
+        State: "CA",   // Tag_V41
+        Zip: "M5C 2V9",   // Tag_V45-V46
+        Phone1: "714",   // Tag_V51
+        Phone2: "966",
+        Phone3: "6605"
     
+        
+        
+        
+    } 
+
+    static Reference_valsB = {
+
+        FirstName: "LHK",  // Tag_V1 
+        lastname: "LHK",    // Tag_V6
+        email_Address: "L@m.com",   //Tag_V11
+        Password: "B3nny!",   // Tag_V16
+        Confirm_Password: "B3nny!",   // Tag_V23
+        Address: "360A",   // Tag_V24
+        Apt_Suite_Unit: "76B",   // Tag_V29
+        Country: "United States",   // Tag_V36
+        City: "San Diego",   // Tag_V37
+        State: "CA",   // Tag_V41
+        Zip: "92046",   // Tag_V45-V46
+        canada_Postal: "K1A 0B1",
+        other_Postal:  "534",
+
+        Phone1: "714",   // Tag_B25-Tag_B26
+        Phone2: "966",   // Tag_B25-Tag_B26
+        Phone3: "6605"   // Tag_B25-Tag_B26
+    }
+
+    static Reference_valsBv2 = {
+
+        FirstName: "LHK",  // Tag_V1 
+        lastname: "LHK",    // Tag_V6
+        email_Address: "L@m.com",   //Tag_V11
+        Password: "B3nny!",   // Tag_V16
+        Confirm_Password: "B3nny!",   // Tag_V23
+        Address: "360A",   // Tag_V24
+        Apt_Suite_Unit: "76B",   // Tag_V29
+        Country: "United States",   // Tag_V36
+        City: "San Diego",   // Tag_V37
+        State: "CA",   // Tag_V41
+        Zip: "92046",   // Tag_V45-V46
+        canada_Postal: "K1A 0B1",
+        other_Postal:  "534",
+
+        Phone1: "963",   // Tag_B25-Tag_B26
+        Phone2: "214",   // Tag_B25-Tag_B26
+        Phone3: "5780"   // Tag_B25-Tag_B26
+    }
+
+
+
+    
+    // First Name positive test cases
     static Testcase_2 = {
     
         TagV2: "Lôren"
@@ -40,200 +128,243 @@ export class Positive_vals {
 
     static Testcase_4 = {
     
-        TagV2: "Lôren"
+        TagV4: "Lo'ren"
 
     }
 
-    static Testcase_2 = {
+    static Testcase_5 = {
     
-        TagV2: "Lôren"
-
-    }
-
-    static Testcase_2 = {
-    
-        TagV2: "Lôren"
-
-    }
-
-    static Testcase_2 = {
-    
-        TagV2: "Lôren"
-
-    }
-
-    static Testcase_2 = {
-    
-        TagV2: "Lôren"
-
-    }
-
-    static Testcase_2 = {
-    
-        TagV2: "Lôren"
-
-    }
-
-    static Testcase_2 = {
-    
-        TagV2: "Lôren"
-
-    }
-
-    static Testcase_2 = {
-    
-        TagV2: "Lôren"
-
-    }
-
-    static Testcase_2 = {
-    
-        TagV2: "Lôren"
-
-    }
-
-    static Testcase_2 = {
-    
-        TagV2: "Lôren"
-
-    }
-
-    static Testcase_2 = {
-    
-        TagV2: "Lôren"
-
-    }
-
-    static Testcase_2 = {
-    
-        TagV2: "Lôren"
-
-    }
-
-    static Testcase_2 = {
-    
-        TagV2: "Lôren"
-
-    }
-
-    static Testcase_2 = {
-    
-        TagV2: "Lôren"
-
-    }
-
-    static Testcase_2 = {
-    
-        TagV2: "Lôren"
-
-    }
-
-    static Testcase_2 = {
-    
-        TagV2: "Lôren"
-
-    }
-
-    static Testcase_2 = {
-    
-        TagV2: "Lôren"
-
-    }
-
-    static Testcase_2 = {
-    
-        TagV2: "Lôren"
-
-    }
-
-    static Testcase_2 = {
-    
-        TagV2: "Lôren"
-
-    }
-
-    static Testcase_2 = {
-    
-        TagV2: "Lôren"
-
-    }
-
-    static Testcase_2 = {
-    
-        TagV2: "Lôren"
-
-    }
-
-    static Testcase_2 = {
-    
-        TagV2: "Lôren"
+        TagV5: "Loren Dean"
 
     }
 
 
-    static Testcase_2 = {
+
+
+
+
+
+    // Last Name Positive Test Cases
+    static Testcase_6 = {
     
-        TagV2: "Lôren"
+        TagV7: "Kïm"
 
     }
 
-    static Testcase_2 = {
+    static Testcase_7 = {
     
-        TagV2: "Lôren"
+        TagV8: "Kim-Hwang"
 
     }
 
-    static Testcase_2 = {
+    static Testcase_8 = {
     
-        TagV2: "Lôren"
+        TagV9: "K'im"
 
     }
 
-    static Testcase_2 = {
+    static Testcase_9 = {
     
-        TagV2: "Lôren"
+        TagV10: "Kim Hwang"
 
     }
 
-    static Testcase_2 = {
+
+
+
+
+
+
+
+
+    // Email Address Positive Test Cases
+    static Testcase_10 = {
     
-        TagV2: "Lôren"
+        TagV12: "BènnyHwang@gmail.com"
 
     }
 
-    static Testcase_2 = {
+    static Testcase_11 = {
     
-        TagV2: "Lôren"
+        TagV13: "Logan$mith@gmail.com"
 
     }
 
-    static Testcase_2 = {
+    static Testcase_12 = {
     
-        TagV2: "Lôren"
+        TagV14: "MrP1bb321@yahoo.com"
 
     }
 
-    static Testcase_2 = {
+    static Testcase_13 = {
     
-        TagV2: "Lôren"
+        TagV15: "KimMitchell@gmail.com"
 
     }
 
-    static Testcase_2 = {
+
+
+
+
+
+
+    // Password Positive Test Cases
+    static Testcase_14 = {
     
-        TagV2: "Lôren"
+        TagV17: "Pas5@#"
 
     }
 
-    static Testcase_2 = {
+    static Testcase_15 = {
     
-        TagV2: "Lôren"
+        TagV18: "Pas5@#"
 
     }
 
-    static Testcase_2 = {
+    static Testcase_16 = {
     
-        TagV2: "Lôren"
+        TagV19: "Pas5@#"
+
+    }
+
+    static Testcase_17 = {
+    
+        TagV20: "Päs5@#"
+
+    }
+
+    static Testcase_18 = {
+    
+        TagV21: "Päs5@#"
+
+    }
+
+    static Testcase_19 = {
+    
+        TagV22: "Pas 5@#"
+
+    }
+
+
+
+
+
+
+
+
+
+    // Address Positive Test Cases
+    static Testcase_20 = {
+    
+        TagV25: "09876 Fröggy Avenue"
+
+    }
+
+    static Testcase_21 = {
+    
+        TagV26: "67584 @xel Road"
+
+    }
+
+    static Testcase_22 = {
+    
+        TagV27: "39201 James-Dean Street"
+
+    }
+
+    static Testcase_23 = {
+    
+        TagV28: "10293 Sugar Mountain Road"
+
+    }
+    
+    
+    
+    
+    
+    
+    // Apt/Suite/Unit 
+    static Testcase_24 = {
+    
+        TagV30: "#J5904"
+
+    }
+
+    static Testcase_25 = {
+    
+        TagV31: "D-150"
+
+    }
+
+
+
+
+
+
+
+    // Other Postal Positive Test Cases, Goes with France_vals
+    static Testcase_28 = {
+    
+        TagV47: "44332"  
+
+    }
+
+    static Testcase_29 = {
+    
+        TagV48: "90210-1234"
+
+    }
+
+    static Testcase_30 = {
+    
+        TagV49: "ÉNG001"
+
+    }
+
+    static Testcase_31 = {
+    
+        TagV50: "GNU 023"
+
+    }
+
+
+
+
+
+
+
+    // City Positive Test Cases 
+    static Testcase_32 = {
+    
+        TagV38: "New-York"
+
+    }
+
+    static Testcase_33 = {
+    
+        TagV39: "Montréal"
+
+    }
+
+    static Testcase_34 = {
+    
+        TagV40: "Los Angeles"
+
+    }
+
+    static Testcase_34v1 = {
+    
+        TagV54: {      // This includes TagV51-TagV53
+            input1: "714", 
+            input2: "966", 
+            input3: "6605", 
+        }
+
+    }
+
+    static Testcase_35 = {
+    
+        TagB: "Lôren"
 
     }
 
